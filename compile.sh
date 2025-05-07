2f45ef976e3a1cd62f43dc70ed6558ea1559bc1e
Christian Tschenett <christian@tschenett.ch>
2025-05-07 22:44:10 +0200

    Towards Reproducible Builds

--- compile.sh
+++ compile.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
 (echo "//multiline" && cat $1 && echo "//<GO2>") | ./interpreter_mac_arm64 | bash
-mv app $2
+
+# Start: Reproducible build (optional)
+cat <<'EOX' > go.mod
+module app
+
+go 1.23.5
+EOX
+export CGO_ENABLED=0
+export GOOS=darwin
+export GOARCH=arm64
+export SOURCE_DATE_EPOCH=1609459200
+go mod tidy
+go build \
+  -trimpath \
+  -ldflags="-buildid= -X main.version=1.0.0 -w -s" \
+  -o app \
+  app.go
+rm go.mod 
+# End: Reproducible build (optional)
+
 rm app.go
+mv app $2
