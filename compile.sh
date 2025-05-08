c2459ad6cff32a1d62947d0ee144efe6c158283c
Christian Tschenett <cht@zuehlke.com>
2025-05-08 12:33:52 +0200

    use docker for go build

--- compile.sh
+++ compile.sh
@@ -3,22 +3,7 @@
 (echo "//multiline" && cat $1 && echo "//<GO2>") | ./interpreter_mac_arm64 | bash
 
 # Start: Reproducible build (optional)
-cat <<'EOX' > go.mod
-module app
-
-go 1.23.5
-EOX
-export CGO_ENABLED=0
-export GOOS=darwin
-export GOARCH=arm64
-export SOURCE_DATE_EPOCH=1609459200
-go mod tidy
-go build \
-  -trimpath \
-  -ldflags="-buildid= -X main.version=1.0.0 -w -s" \
-  -o app \
-  app.go
-rm go.mod 
+docker run --rm -v "$PWD":/src -w /src -e SOURCE_DATE_EPOCH=1609459200 -e GOARCH=arm64 -e GOOS=darwin -e CGO_ENABLED=0 golang:1.23.5 sh -c 'go build -trimpath -ldflags="-buildid= -X main.version=1.0.0 -w -s" app.go'
 # End: Reproducible build (optional)
 
 rm app.go
