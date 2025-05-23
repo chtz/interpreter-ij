bf5da14c279bb8115bb768aed39cc54ce6e163ce
Christian Tschenett <christian@tschenett.ch>
2025-05-23 23:26:43 +0200

    wip

--- mcp.sh
+++ mcp.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-cd /Users/chrigi/git/interpreter-ij
+
+# Claude Desktop Config
+#{
+#  "mcpServers": {
+#    "ijscript": {
+#      "command": "/.../mcp.sh",
+#      "args": [],
+#      "transport": {
+#        "type": "stdio"
+#      }
+#    }
+#  }
+#}
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $SCRIPT_DIR
 cat interpreter.s|./until.rb "interpreter is ready" > interpreter_base.s
 cat interpreter_base.s eval.s mcp.s > mcp_eval.s
 ./native_interpreter.sh mcp_eval.s
